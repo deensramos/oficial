041bb859ccf971eac96ae49fb4ab4c8959da7697
deensramos <52183262+deensramos@users.noreply.github.com>
2022-07-13 21:17:02 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+            // Open and close sidebar
+            function openNav() {
+              document
+                  .getElementById("mySidebar")
+                  .style
+                  .width = "60%";
+              document
+                  .getElementById("mySidebar")
+                  .style
+                  .display = "block";
+          }
+
+          function closeNav() {
+              document
+                  .getElementById("mySidebar")
+                  .style
+                  .display = "none";
+          }
